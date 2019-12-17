10fca55dbbe0a7f58036cc6426a3f3f9e9bf3da5
zhulichen <390597658@qq.com>
2019-12-17 18:58:36 +0800

    添加跳过按钮

--- dictationhelper/app/src/main/java/cn/edu/hebtu/software/listendemo/Login/Splash.java
+++ dictationhelper/app/src/main/java/cn/edu/hebtu/software/listendemo/Login/Splash.java
@@ -5,8 +5,12 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import cn.edu.hebtu.software.listendemo.Host.index.ListenIndexActivity;
 import cn.edu.hebtu.software.listendemo.R;
 import cn.edu.hebtu.software.listendemo.Untils.Constant;
@@ -14,6 +18,9 @@ import cn.edu.hebtu.software.listendemo.Untils.StatusBarUtil;
 
 public class Splash extends AppCompatActivity {
 
+
+    @BindView(R.id.btn_skip)
+    Button btnSkip;
     private SharedPreferences sp;
     private int jumpType = 0;
 
@@ -21,12 +28,16 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        ButterKnife.bind(this);
         Thread myThread = new Thread() {//创建子线程
             @Override
             public void run() {
                 try {
                     sleep(1500);//使程序休眠五秒
 
+                    if(jumpType == -1){
+                        return;
+                    }
                     if(jumpType == 1){
                         Intent intent = new Intent(getApplicationContext(), ListenIndexActivity.class);
                         startActivity(intent);
@@ -48,6 +59,23 @@ public class Splash extends AppCompatActivity {
         } else {
 
         }
+        btnSkip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(myThread.isAlive()){
+                    jumpType=-1;
+                    if(jumpType == 1){
+                        Intent intent = new Intent(getApplicationContext(), ListenIndexActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }else{
+                        Intent it = new Intent(getApplicationContext(), LoginActivity.class);//启动MainActivity
+                        startActivity(it);
+                        finish();//关闭当前活动
+                    }
+                }
+            }
+        });
         StatusBarUtil.transparencyBar(this);
         myThread.start();//启动线程
     }
